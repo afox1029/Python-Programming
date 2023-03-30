b6a9b8077307e404e2ce1ed223b321a07fc1f12c
afox1029 <114184318+afox1029@users.noreply.github.com>
2023-03-30 13:24:52 -0400

    Add files via upload

--- M2HW_JonesAlexander.py
+++ M2HW_JonesAlexander.py
@@ -1,33 +1,50 @@
-# CSC121
-# M2HW
+# Program that quizzes user on countrys capitals
+# 3/28/2023
+# CSC221 M2HW – Country Capitals
 # Alexander Jones
-# 1/26/2023
-#
-# Input amount
-# Display "Your change is"
-# If statment for "no change"
-# If statement to get and display quarter amount
-# If statement to get and display dime amount
-# If statement to get and display nickel amount
-# If statement to get and display penny amount
-# Else that displays total
+#Create a holder for the capitals and countries
+#loop a question asking a random capital
+#Count all of the answers
+#Exit the program when told to quit
 
-import math
+import random
 
-total = int(input("Enter the amount of change out of 100: "))
-print("Your change is:")
-if total >= 25:
-    quarters = total // 25
-    total = total % 25
-    print(quarters, " quarters")
-if total >= 10:
-    dimes = total // 10
-    total = total % 10
-    print(dimes, " dimes")
-if total >= 5:
-    nickels = total // 5
-    total = total % 5
-    print(nickels, " nickels")
-if total >= 1:
-    pennies = total // 1
-    print(pennies, " pennies")
+capitals = {
+    "Argentina": "Buenos Aires",
+    "Brazil": "Brasília",
+    "Canada": "Ottawa",
+    "China": "Beijing",
+    "France": "Paris",
+    "Germany": "Berlin",
+    "India": "New Delhi",
+    "Italy": "Rome",
+    "Japan": "Tokyo",
+    "Mexico": "Mexico City",
+    "Nigeria": "Abuja",
+    "Russia": "Moscow",
+    "South Africa": "Pretoria (administrative); Cape Town (legislative); Bloemfontein (judiciary)",
+    "Spain": "Madrid",
+    "Switzerland": "Bern",
+    "Thailand": "Bangkok",
+    "Turkey": "Ankara",
+    "United Kingdom": "London",
+    "United States": "Washington, D.C."
+}
+
+correct = 0
+incorrect = 0
+
+while True:
+    country = random.choice(list(capitals.keys()))
+    capital = capitals[country]
+    answer = input("What is the capital of " + country + "? ")
+    if answer == "quit":
+        break
+    elif answer == capital:
+        print("Correct!")
+        correct += 1
+    else:
+        print("Incorrect. The capital of " + country + " is " + capital + ".")
+        incorrect += 1
+
+print("You got", correct, "correct and", incorrect, "incorrect.")
