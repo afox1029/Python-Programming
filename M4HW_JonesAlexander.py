0647dc04fe48f7adeb98f663837bc43dc338d0ed
afox1029 <114184318+afox1029@users.noreply.github.com>
2023-04-04 13:39:40 -0400

    Add files via upload

--- M4HW_JonesAlexander.py
+++ M4HW_JonesAlexander.py
@@ -1,91 +1,34 @@
-# The prpgram gives quick math quizzes and displays random numbers
-# 2/2/2023
-# CSC121 M4HW - Math Quiz
+# Program that puts data on students into  a table
+# 4/4/2023
+# CSC221 M4HW – Students Class
 # Alexander Jones
 #
-import random
+# Make a place to store student data
+# Create three Student objects
+# Display data for each student as a table
 
-print("Welcome to Math Quiz")
-print()
-print()
-print()
-print()
+class Student:
+    def __init__(self, id_number, first_name, last_name, major):
+        self.id_number = id_number
+        self.first_name = first_name
+        self.last_name = last_name
+        self.major = major
 
-def display_menu():
-    print("MAIN MENU")
-    print()
-    print("----------------")
-    print("1) Add Random Numbers")
-    print("2) Subtract Random Numbers")
-    print("3) Exit")
-    print()
-    
-def input_1():
-    x = 1
-    try_again = "y"
-    while try_again == "y":
-        r1 = random.randint(10, 100)
-        r2 = random.randint(10, 100)
-        total = r1 + r2
-        print(" ", r1)
-        print("+", r2)
-        choice_1 = int(input("Enter answer. "))
-        while choice_1 != total:
-            if choice_1 > total:
-                print("Sorry, guess is too high.")
-                choice_1 = int(input("Try again: "))
-                x += 1
-            else:
-                print("Sorry, guess is too low.")
-                choice_1 = int(input("Try again: "))
-                x += 1
-                
-        print()
-        print("Congratulations!!!! Your answer is correct.")
-        print("Number of guesses: ", str(x))
-        try_again = "n"
+    def __str__(self):
+        return f"{self.id_number:5} {self.first_name:18} {self.last_name:12} {self.major:10}"
 
-def input_2():
-    x = 1
-    try_again = "y"
-    while try_again == "y":
-        r1 = random.randint(10, 100)
-        r2 = random.randint(10, 100)
-        total = r1 - r2
-        print(" ", r1)
-        print("-", r2)
-        choice_1 = int(input("Enter answer. "))
-        while choice_1 != total:
-            if choice_1 > total:
-                print("Sorry, guess is too high.")
-                choice_1 = int(input("Try again: "))
-                x += 1
-            else:
-                print("Sorry, guess is too low.")
-                choice_1 = int(input("Try again: "))
-                x += 1
-                
-        print()
-        print("Congratulations!!!! Your answer is correct.")
-        print("Number of guesses: ", str(x))
-        print()
-        try_again = "n"
 
-again = "y"
 
-while again == "y":
-    display_menu()
-    user_input = input("Please choose one of the menu options: ")
-    if user_input == "1":
-        input_1()
-    elif user_input == "2":
-        input_2()
-    elif user_input =="3":
-        print("Thank you for playing...")
-        print("Bye!!")
-        break
-    else:
-        print("That is not a menu option.")
-        print()
-        
-        
+student1 = Student(47899, "      Susan", "Meyers", "Accounting")
+student2 = Student(39119, "      Mark", "Jones", "Programmer")
+student3 = Student(81774, "      Joy", "Rogers", "Engineering")
+
+
+table_headers = "ID Number   First Name   Last Name    Major"
+print(table_headers)
+print("-" * len(table_headers))
+
+print(student1)
+print(student2)
+print(student3)
+print("-" * len(table_headers))
